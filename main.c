2930bb14cae5831831a0138f35fc761bd4e4ae3c
renatoleonholberton <2829@holbertonschool.com>
2021-05-11 20:48:58 +0000

    Create main file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: 0 on success, err code otherwise
+*/
+int main()
+{
+	return (0);
+}
