77f529b1dc73ddef758404de2da8799fd037e2ca
renatoleonholberton <2829@holbertonschool.com>
2021-05-12 05:41:32 +0000

    Create opcode function handler

---
+++ handle_opcode.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+int handle_opcode(manager_t manager, char *opcode, int line_num)
+{
+	int i;
+	instruction_t instructions[] = {
+		{"pall", print_all},
+		{NULL, NULL}
+	};
+
+	for (i = 0; instructions[i].opcode != NULL; i++)
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(&(manager.stack_tail), line_num);
+			return (1);
+		}
+
+	return (0);
+}
