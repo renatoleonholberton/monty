79c5f25495029ed18859460b393377d3677db612
renatoleonholberton <2829@holbertonschool.com>
2021-05-12 17:37:04 +0000

    Add err_pop function

--- error_funcs_2.c
+++ error_funcs_2.c
@@ -14,3 +14,17 @@ void err_pint(int line_number)
 	exit(EXIT_FAILURE);
 }
 
+
+void err_pop(int line_number)
+{/* L<line_number>: can't pop an empty stack */
+	char *msg;
+
+	write(2, "L", 1);
+	msg = num_to_str(line_number);
+	write(2, msg, _strlen(msg));
+	free(msg);
+	msg = ": can't pop an empty stack\n";
+	write(2, msg, _strlen(msg));
+	cleanup();
+	exit(EXIT_FAILURE);
+}
