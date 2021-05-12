6e999cf5ed5291be968101f87a321d09731c0286
renatoleonholberton <2829@holbertonschool.com>
2021-05-12 15:22:33 +0000

    Create second set of error functions

---
+++ error_funcs_2.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+void err_pint(int line_number)
+{/* L<line_number>: can't pint, stack empty */
+	char *msg;
+
+	write(2, "L", 1);
+	msg = num_to_str(line_number);
+	write(2, msg, _strlen(msg));
+	free(msg);
+	msg = ": can't pint, stack empty\n";
+	write(2, msg, _strlen(msg));
+	cleanup();
+	exit(EXIT_FAILURE);
+}
+
