9ca51a0dac3429b50003d6f6ceb3cd12e6b65a1c
renatoleonholberton <2829@holbertonschool.com>
2021-05-12 15:20:08 +0000

    Crate pint function

--- op_funcs.c
+++ op_funcs.c
@@ -14,3 +14,19 @@ void print_all(stack_t **stack_tail, unsigned int __attribute__((unused))line_nu
 		tail = tail->prev;
 	}
 }
+
+/**
+ * pint - Prints the value at the top of the stack
+ * @stack_tail: Double pointer to the stack's tail
+ * @line_number: Number of line instruction
+*/
+void pint(stack_t **stack_tail, unsigned int line_number)
+{
+	stack_t *tail;
+
+	if (stack_tail == NULL || *stack_tail == NULL)
+		err_pint(line_number);
+	
+	tail = *stack_tail;
+	printf("%d\n", tail->n);
+}
