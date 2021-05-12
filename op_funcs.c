3bcdd2cbfe7c3ce3ac20dd1eb26a1972e30ef396
renatoleonholberton <2829@holbertonschool.com>
2021-05-12 17:40:39 +0000

    Update code and create pop function

--- op_funcs.c
+++ op_funcs.c
@@ -1,17 +1,20 @@
 #include "monty.h"
 
-void print_all(stack_t **stack_tail, unsigned int __attribute__((unused))line_number)
+void print_all(stack_t **head, unsigned int __attribute__((unused))line_number)
 {
-	stack_t *tail;
+	stack_t *curr;
 
-	if (stack_tail == NULL || *stack_tail == NULL)
+	if (head == NULL || *head == NULL)
 		return;
 	
-	tail = *stack_tail;
-	while (tail != NULL)
+	curr = *head;
+	while (curr->next != NULL)
+		curr = curr->next;
+
+	while (curr != NULL)
 	{
-		printf("%d\n", tail->n);
-		tail = tail->prev;
+		printf("%d\n", curr->n);
+		curr = curr->prev;
 	}
 }
 
@@ -20,13 +23,44 @@ void print_all(stack_t **stack_tail, unsigned int __attribute__((unused))line_nu
  * @stack_tail: Double pointer to the stack's tail
  * @line_number: Number of line instruction
 */
-void pint(stack_t **stack_tail, unsigned int line_number)
+void pint(stack_t **head, unsigned int line_number)
 {
-	stack_t *tail;
+	stack_t *curr;
 
-	if (stack_tail == NULL || *stack_tail == NULL)
+	if (head == NULL || *head == NULL)
 		err_pint(line_number);
-	
-	tail = *stack_tail;
-	printf("%d\n", tail->n);
+
+	curr = *head;
+	while (curr->next != NULL)
+		curr = curr->next;
+
+	printf("%d\n", curr->n);
+}
+
+/**
+ * pop - Removes the node at the top of the stack
+ * @head: Double pointer to the stack's head
+ * @line_number: Number of line instruction
+*/
+void pop(stack_t **head, unsigned int line_number)
+{
+	stack_t *curr, *prev = NULL;
+
+	if (head == NULL || *head == NULL)
+		err_pop(line_number);
+
+	if ((*head)->next == NULL)
+	{
+		free(*head);
+		*head = NULL;
+		return;
+	}
+		
+	for (curr = *head; curr->next != NULL; curr = curr->next)
+		prev = curr;
+
+	if (prev != NULL)
+		prev->next = NULL;
+	curr->prev = NULL;
+	free(curr);
 }
