6de73e7860ec5df87e334de587c10fba8c8a0f77
renatoleonholberton <2829@holbertonschool.com>
2021-05-12 05:39:13 +0000

    Structure main code

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+manager_t manager = {NULL, NULL, 0};
+
 /**
  * main - Entry point
  * @argc: Number of arguments passed to the program
@@ -7,7 +9,17 @@
  *
  * Return: 0 on success, err code otherwise
 */
-int main()
+int main(int argc, char **argv)
 {
+	if (argc != 2)
+		err_argc();
+
+	manager.file = fopen(argv[1], "r");
+	if (manager.file == NULL)
+		err_open_file(argv[1]);
+
+	parse_input_file();
+
+	cleanup();
 	return (0);
 }
