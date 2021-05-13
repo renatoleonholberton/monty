b5b958c8869aafafbddf47f9f0e03b966049b708
renatoleonholberton <2829@holbertonschool.com>
2021-05-13 03:21:51 +0000

    Create err_swap function

--- error_funcs_2.c
+++ error_funcs_2.c
@@ -35,3 +35,22 @@ void err_pop(int line_number)
 	cleanup();
 	exit(EXIT_FAILURE);
 }
+
+
+/**
+ * err_pop - Prints an error to stderr
+ * @line_number: Number of instruction line
+*/
+void err_pop(int line_number)
+{/* L<line_number>: can't swap, stack too short */
+	char *msg;
+
+	write(2, "L", 1);
+	msg = num_to_str(line_number);
+	write(2, msg, strlen(msg));
+	free(msg);
+	msg = ": can't swap, stack too short\n";
+	write(2, msg, strlen(msg));
+	cleanup();
+	exit(EXIT_FAILURE);
+}
--- monty.h
+++ monty.h
@@ -83,6 +83,7 @@ void err_malloc(void);
 /* errors 2 */
 void err_pint(int line_number);
 void err_pop(int line_number);
+void err_pop(int line_number);
 
 /* utils */
 int is_number(char *str);
